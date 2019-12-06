8eacbbc261a308db9f331e8dd82ed300a8accd3a
Alex Ball <ball.alex.509@gmail.com>
2019-12-05 19:42:22 -0500

    Create top-level application UI

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import SearchForm from "./SearchForm";
+import RateQuoteClient from "./RateQuoteClient";
+import RateQuoteDisplay from "./RateQuoteDisplay";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.rateQuoteClient = new RateQuoteClient();
+    this.state = {};
+  }
+
+  requestQuotes({ creditScore, occupancy, loanSize, propertyType }) {
+    this.rateQuoteClient
+      .getRateQuotes({
+        creditScore,
+        occupancy,
+        loanSize,
+        propertyType
+      })
+      .then(quotes => {
+        this.setState({ quotes });
+      })
+      .catch(reason => {
+        console.error("Couldn't request quotes", reason);
+      });
+  }
+
+  render() {
+    const quotes = this.state.quotes;
+    return (
+      <div>
+        <SearchForm
+          onQuoteRequested={formInfo => this.requestQuotes(formInfo)}
+        />
+        {!!quotes && <RateQuoteDisplay quotes={quotes} />}
+      </div>
+    );
+  }
 }
 
 export default App;
